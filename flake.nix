23c54e702861c314bc32c0b029393581233ae00f
John Schmidt <john@schmidthaus.rocks>
2026-03-30 10:06:23 -0700

    Update the prompt. Formatting changes.
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- flake.nix
+++ flake.nix
@@ -19,32 +19,45 @@
         };
         pname = "blincus";
         wrapScript = name: pkgs.writeShellScriptBin name (builtins.readFile ./${name});
-        generateRunnablePackage = { name, dependencies }:
+        generateRunnablePackage = {
+          name,
+          dependencies,
+        }:
           pkgs.symlinkJoin {
             name = "${name}";
-            paths = if dependencies != null then [(wrapScript name)] ++ dependencies else [(wrapScript name)];
+            paths =
+              if dependencies != null
+              then [(wrapScript name)] ++ dependencies
+              else [(wrapScript name)];
             buildInputs = [pkgs.makeWrapper];
-            postBuild = if name == pname then "wrapProgram $out/bin/${name} --prefix PATH : $out/bin" else "wrapProgram $out/bin/${name} --prefix PATH : $out/bin/${pname}-${name}";
+            postBuild =
+              if name == pname
+              then "wrapProgram $out/bin/${name} --prefix PATH : $out/bin"
+              else "wrapProgram $out/bin/${name} --prefix PATH : $out/bin/${pname}-${name}";
           };
       in {
         packages = {
           default = self.packages.${system}.blincus;
-          ${pname} = generateRunnablePackage { 
+          ${pname} = generateRunnablePackage {
             name = "${pname}";
-            dependencies = (with pkgs; [incus jq xorg.xhost coreutils gnugrep gnused getent util-linux dconf coreutils-full]);
+            dependencies = with pkgs; [incus jq xorg.xhost coreutils gnugrep gnused getent util-linux dconf coreutils-full];
           };
           install = generateRunnablePackage {
             name = "install";
-            dependencies = (with pkgs; [curl wget gnutar coreutils]);
+            dependencies = with pkgs; [curl wget gnutar coreutils];
           };
-          uninstall = generateRunnablePackage { 
-            name = "uninstall"; 
+          uninstall = generateRunnablePackage {
+            name = "uninstall";
             dependencies = with pkgs; [coreutils];
           };
         };
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [shellcheck nodePackages.bash-language-server];
+          shellHook = ''
+            echo "Entering Nix dev shell"
+            export PS1="[nix-shell] $PS1"
+          '';
         };
 
         formatter = pkgs.alejandra;
